3c2040c105711cbeb4ac9aeb8c854aebe15cb803
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-04-16 17:43:35 +0200

    add api folder

---
+++ api/v1/app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from flask import Flask
+from api import app
+from models import storage
+from api.v1.views import app_views
+import os
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """Handles teardown of the application."""
+    storage.close()
+
+if __name__ == "__main__":
+    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(os.getenv("HBNB_API_PORT", "5000"))
+    app.run(host=host, port=port, threaded=True)

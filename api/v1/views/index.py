602d25ae744598d14a61b831eff1925d282da7ed
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-04-17 11:46:15 +0200

    modified folder

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-from flask import jsonify
 from api.v1.views import app_views
+from flask import jsonify
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status')
 def get_status():
     return jsonify({"status": "OK"})

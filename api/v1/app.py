2546fa1fd4e9565fccbbbb6569e0cf09407773b7
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-04-17 09:36:12 +0200

    create a handler for 404 errors that returns a JSON-formatted 404 status code response

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,12 +4,22 @@ from api import app
 from models import storage
 from api.v1.views import app_views
 import os
+from flask import jsonify
+from . import app_views
+
 
 
 @app.teardown_appcontext
 def teardown(exception):
     """Handles teardown of the application."""
     storage.close()
+
+@app_views.app_errorhandler(404)
+def not_found(error):
+    """Handles 404 Not Found errors."""
+    response = jsonify({"erreur": "Non trouvé"})
+    response.status_code = 404
+    return response
 
 if __name__ == "__main__":
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")

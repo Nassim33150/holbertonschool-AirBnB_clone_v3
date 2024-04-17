a8150e9322b82e16b611e61cca67e20cf43acf69
root <root@LAPTOP-2LV4F0PM>
2024-04-17 10:56:29 +0200

    api

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+""" import Blueprint from flask doc """
 from flask import Blueprint
 
-app_views = Blueprint('app_views', __name__)
 
-from api.v1.views.index import *
+""" Create a Blueprint object """
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,35 @@
 #!/usr/bin/python3
 from flask import jsonify
+from flask import Blueprint
 from api.v1.views import app_views
+from models import storage
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models.amenity import Amenity
+
+
+""" Status of the API """
+
 
 @app_views.route('/status', methods=['GET'])
 def get_status():
     return jsonify({"status": "OK"})
+
+
+""" Stats of the API """
+
+
+@app_views.route('/stats', methods=['GET'])
+def get_stats():
+    stats = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
+    }
+    return jsonify(stats)

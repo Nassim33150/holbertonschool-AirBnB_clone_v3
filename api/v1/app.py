4f5dcefe40fb82558faf5b4374b32736122ed197
root <root@LAPTOP-2LV4F0PM>
2024-04-17 11:07:17 +0200

    api

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 from flask import Flask
-from api import app
 from models import storage
 from api.v1.views import app_views
 import os
 from flask import jsonify
-from . import app_views
 
 
+app = Flask(__name__)
+
+app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def teardown(exception):

809647f6aef29e342e17402d32dcbb906518d49f
root <root@LAPTOP-2LV4F0PM>
2024-04-17 19:55:52 +0200

    update init & route amenities

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,6 +2,7 @@
 """ import modules """
 from api.v1.views import states
 from api.v1.views import cities
+from api.v1.views import amenities
 from flask import Blueprint
 from api.v1.views.index import *
 

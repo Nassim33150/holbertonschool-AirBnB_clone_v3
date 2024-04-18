4c3030c0a2aaf0f9a0b7faf9585652e7458b9dba
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-04-18 17:08:00 +0200

    resolve conflits

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,24 +1,4 @@
-#!/usr/bin/python3
-
-from flask import Flask
-from models import storage
-import os
-from api.v1.views import app_views
-
-
-app = Flask(__name__)
-
-app.register_blueprint(app_views)
-
-@app.teardown_appcontext
-def teardown(exception):
-    """Handles teardown of the application."""
-    storage.close()
-
-if __name__ == "__main__":
-    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.getenv("HBNB_API_PORT", "5000"))
-    app.run(host=host, port=port, threaded=True)
+# api/v1/app.py
 """ import modules """
 from flask import Flask
 from models import storage
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,14 +1,5 @@
 #!/usr/bin/python3
 """ import Blueprint from flask doc """
-<<<<<<< HEAD
-from flask import Blueprint
-
-
-""" Create a Blueprint object """
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-from api.v1.views.index import *
-=======
 """ import modules """
 from api.v1.views import states
 from api.v1.views import cities
@@ -17,5 +8,3 @@ from flask import Blueprint
 from api.v1.views.index import *
 
 app_views = Blueprint('app_views', __name__)
-
->>>>>>> 66aceb74c43ec53b6f3f97318bd7bccf3082a81c

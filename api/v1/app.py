261b8b9ec199f0c5876eca0dbf9fca9eb2357e42
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-04-18 17:18:27 +0200

    resolve conflits

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,25 +1,22 @@
-# api/v1/app.py
+#!/usr/bin/python3
 """ import modules """
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
-import os
-from flask import jsonify
 
-""" Create a new Flask instance """
+# Create Flask instance
 app = Flask(__name__)
 
-""" Close the session """
+# Register blueprint
+app.register_blueprint(app_views, url_prefix="/api/v1")
+
+# Teardown method
 @app.teardown_appcontext
-def teardown_appcontext(exception):
+def teardown(exception):
     storage.close()
 
-""" Create a 404 error """
-@app.errorhandler(404)
-def not_found(error):
-    return jsonify({"error": "Not found"}), 404
-
+# Run Flask server
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
+    host = environ.get('HBNB_API_HOST', '0.0.0.0')
+    port = int(environ.get('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
